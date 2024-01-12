49150f4ac428f1f53f6fb2f6752aaefbb6de84bf
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-01-12 12:15:18 +0000

    Add files via upload

--- libft/incs/get_next_line.h
+++ libft/incs/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmaria-d <mmaria-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 19:30:35 by mmaria-d          #+#    #+#             */
-/*   Updated: 2023/09/16 12:45:49 by mmaria-d         ###   ########.fr       */
+/*   Updated: 2024/01/12 12:00:57 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,29 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <stdbool.h>
 
 # include "ft_memfunc.h"
 # include "error_handling.h"
 
-/* get_next_line.c*/
-char	*get_next_line(int fd);
-char	*get_next_line_multi(int fd);
+typedef struct s_gnl_len
+{
+	char	*line;
+	int		len;
+	t_uchar	status;
+}	t_gnl_len;
 
+enum	e_gnl
+{
+	GNL_OK,
+	GNL_FAILURE,
+	GNL_BAD_FD,
+	GNL_EOF,
+};
+
+/* get_next_line.c*/
+char		*get_next_line(int fd);
+char		*get_next_line_multi(int fd);
+t_gnl_len	*get_next_line_len(int fd);
+void		free_gnl_len(void *gnl);
 #endif
