c4a71e4d6f6a2387ccdc9d981028133dfdb807ff
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-02-06 10:07:06 +0000

    Add files via upload

--- libft/srcs/hashtable/hashtable_utils.c
+++ libft/srcs/hashtable/hashtable_utils.c
@@ -3,31 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   hashtable_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 10:59:09 by codespace         #+#    #+#             */
-/*   Updated: 2023/09/20 11:28:36 by codespace        ###   ########.fr       */
+/*   Updated: 2024/02/06 09:45:30 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hashtable.h"
 
-void	*hashtable_return_value(t_hashtable *table, void *key)
+int	hashtable_return_value(t_hashtable *table, void *key, void *place_res)
 {
 	int			index;
 	t_htnode	*cur;
 
 	if (!table)
-		return (NULL);
+		return (0);
 	index = table->hash_func(key) % table->size;
 	if (!table->collision[index])
-		return (NULL);
+		return (0);
 	cur = table->collision[index]->head;
 	while (cur)
 	{
 		if (!table->key_compare(key, cur->key))
-			return (cur->value);
+		{
+			*(void **)place_res = cur->value;
+			return (1);
+		}
 		cur = cur->next;
 	}
-	return (NULL);
+	return (0);
 }
