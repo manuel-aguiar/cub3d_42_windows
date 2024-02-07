56e2723c368409779cd63eebef1ce1a742e7ccbc
Manuel Aguiar <113345121+manuel-aguiar@users.noreply.github.com>
2024-02-07 15:21:05 +0000

    Add files via upload

--- srcs/game/map_player_collisions.c
+++ srcs/game/map_player_collisions.c
@@ -19,7 +19,6 @@ https://github.com/OneLoneCoder/Javidx9/blob/master/PixelGameEngine/SmallerProje
 void	handle_collisions(t_game *game, t_vector potencial)
 {
 	t_player	*player;
-	t_vector	potencial_posi;
 	t_map		*map;
 	int			fixed_x;
 	int			fixed_y;
@@ -40,7 +39,7 @@ void	handle_collisions(t_game *game, t_vector potencial)
 		potencial = vector_multi(vector_norm(potencial, pot_len), player->unit_size - 0.001f);
 	}
 
-	potencial_posi = vector_add(player->map_posi, potencial);
+	player->map_posi = vector_add(player->map_posi, potencial);
 
 
 	
@@ -51,92 +50,95 @@ void	handle_collisions(t_game *game, t_vector potencial)
 	//check left
 
 
-	player->map_posi = potencial_posi;
+		
 
-	if (map->map[(fixed_x - 1) + fixed_y * map->width] == '1')
-		player->map_posi.x = ft_fmax(player->map_posi.x, fixed_x + (player->unit_size));
-	if (map->map[(fixed_x + 1) + fixed_y * map->width] == '1')
-		player->map_posi.x = ft_fmin(player->map_posi.x, fixed_x + 1 - (player->unit_size));
-	if (map->map[fixed_x + (fixed_y + 1) * map->width] == '1')
-		player->map_posi.y = ft_fmin(player->map_posi.y, fixed_y + 1 - (player->unit_size));
-	if (map->map[fixed_x + (fixed_y - 1) * map->width] == '1')
-		player->map_posi.y = ft_fmax(player->map_posi.y, fixed_y + (player->unit_size));
+		if (map->map[(fixed_x - 1) + fixed_y * map->width] == '1')
+			player->map_posi.x = ft_fmax(player->map_posi.x, fixed_x + (player->unit_size));
+		if (map->map[(fixed_x + 1) + fixed_y * map->width] == '1')
+			player->map_posi.x = ft_fmin(player->map_posi.x, fixed_x + 1 - (player->unit_size));
+		if (map->map[fixed_x + (fixed_y + 1) * map->width] == '1')
+			player->map_posi.y = ft_fmin(player->map_posi.y, fixed_y + 1 - (player->unit_size));
+		if (map->map[fixed_x + (fixed_y - 1) * map->width] == '1')
+			player->map_posi.y = ft_fmax(player->map_posi.y, fixed_y + (player->unit_size));
 
 
-	t_vector nearest;
-	t_vector ray_to_nearest;
-	float	ray_length;
-	float	overlap;
-	
+		t_vector nearest;
+		t_vector ray_to_nearest;
+		float	ray_length;
+		float	overlap;
+		
 
-	//check topo left
-	//printf("fixed x %d, fixed y %d, index top left %d\n", fixed_x, fixed_y,(fixed_x - 1) + (fixed_y + 1) * map->width);
-	if (map->map[(fixed_x - 1) + (fixed_y + 1) * map->width] == '1')
-	{
-		//printf("posi start (%.3f, %.3f)\n", player->map_posi.x, player->map_posi.y);
-		nearest.x = ft_fmin(player->map_posi.x, (float)fixed_x);
-		nearest.y = ft_fmax(player->map_posi.y, (float)(fixed_y + 1));
-		ray_to_nearest = vector_sub(nearest,  player->map_posi);
-		ray_length = vector_len(ray_to_nearest);
-
-		overlap = player->unit_size - ray_length;
-		if (overlap > 0)
+		//check topo left
+		//printf("fixed x %d, fixed y %d, index top left %d\n", fixed_x, fixed_y,(fixed_x - 1) + (fixed_y + 1) * map->width);
+		if (map->map[(fixed_x - 1) + (fixed_y + 1) * map->width] == '1')
 		{
-			ray_to_nearest = vector_norm(ray_to_nearest, ray_length);
-			player->map_posi = vector_sub(player->map_posi, vector_multi(ray_to_nearest, overlap));
-		}
-		//printf("posi end (%.3f, %.3f)\n", player->map_posi.x, player->map_posi.y);
-	}
-	//top right
-	if (map->map[(fixed_x + 1) + (fixed_y + 1) * map->width] == '1')
-	{
-		//printf("posi start (%.3f, %.3f)\n", player->map_posi.x, player->map_posi.y);
-		nearest.x = ft_fmax(player->map_posi.x, (float)(fixed_x + 1));
-		nearest.y = ft_fmax(player->map_posi.y, (float)(fixed_y + 1));
-		ray_to_nearest = vector_sub(nearest,  player->map_posi);
-		ray_length = vector_len(ray_to_nearest);
+			//printf("posi start (%.3f, %.3f)\n", player->map_posi.x, player->map_posi.y);
+			nearest.x = ft_fmin(player->map_posi.x, (float)fixed_x);
+			nearest.y = ft_fmax(player->map_posi.y, (float)(fixed_y + 1));
+			ray_to_nearest = vector_sub(nearest,  player->map_posi);
+			ray_length = vector_len(ray_to_nearest);
 
-		overlap = player->unit_size - ray_length;
-		if (overlap > 0)
+			overlap = player->unit_size - ray_length;
+			if (overlap > 0)
+			{
+				ray_to_nearest = vector_norm(ray_to_nearest, ray_length);
+				player->map_posi = vector_sub(player->map_posi, vector_multi(ray_to_nearest, overlap));
+			}
+			//printf("posi end (%.3f, %.3f)\n", player->map_posi.x, player->map_posi.y);
+		}
+		//top right
+		if (map->map[(fixed_x + 1) + (fixed_y + 1) * map->width] == '1')
 		{
-			ray_to_nearest = vector_norm(ray_to_nearest, ray_length);
-			player->map_posi = vector_sub(player->map_posi, vector_multi(ray_to_nearest, overlap));
+			//printf("posi start (%.3f, %.3f)\n", player->map_posi.x, player->map_posi.y);
+			nearest.x = ft_fmax(player->map_posi.x, (float)(fixed_x + 1));
+			nearest.y = ft_fmax(player->map_posi.y, (float)(fixed_y + 1));
+			ray_to_nearest = vector_sub(nearest,  player->map_posi);
+			ray_length = vector_len(ray_to_nearest);
+
+			overlap = player->unit_size - ray_length;
+			if (overlap > 0)
+			{
+				ray_to_nearest = vector_norm(ray_to_nearest, ray_length);
+				player->map_posi = vector_sub(player->map_posi, vector_multi(ray_to_nearest, overlap));
+			}
 		}
-	}
 
-	//bot left
-	if (map->map[(fixed_x - 1) + (fixed_y - 1) * map->width] == '1')
-	{
-		//printf("posi start (%.3f, %.3f)\n", player->map_posi.x, player->map_posi.y);
-		nearest.x = ft_fmin(player->map_posi.x, (float)(fixed_x));
-		nearest.y = ft_fmin(player->map_posi.y, (float)(fixed_y));
-		ray_to_nearest = vector_sub(nearest,  player->map_posi);
-		ray_length = vector_len(ray_to_nearest);
-
-		overlap = player->unit_size - ray_length;
-		if (overlap > 0)
+		//bot left
+		if (map->map[(fixed_x - 1) + (fixed_y - 1) * map->width] == '1')
 		{
-			ray_to_nearest = vector_norm(ray_to_nearest, ray_length);
-			player->map_posi = vector_sub(player->map_posi, vector_multi(ray_to_nearest, overlap));
+			//printf("posi start (%.3f, %.3f)\n", player->map_posi.x, player->map_posi.y);
+			nearest.x = ft_fmin(player->map_posi.x, (float)(fixed_x));
+			nearest.y = ft_fmin(player->map_posi.y, (float)(fixed_y));
+			ray_to_nearest = vector_sub(nearest,  player->map_posi);
+			ray_length = vector_len(ray_to_nearest);
+
+			overlap = player->unit_size - ray_length;
+			if (overlap > 0)
+			{
+				ray_to_nearest = vector_norm(ray_to_nearest, ray_length);
+				player->map_posi = vector_sub(player->map_posi, vector_multi(ray_to_nearest, overlap));
+			}
 		}
-	}
 
-	//bot right
-	if (map->map[(fixed_x + 1) + (fixed_y - 1) * map->width] == '1')
-	{
-		//printf("posi start (%.3f, %.3f)\n", player->map_posi.x, player->map_posi.y);
-		nearest.x = ft_fmax(player->map_posi.x, (float)(fixed_x + 1));
-		nearest.y = ft_fmin(player->map_posi.y, (float)(fixed_y));
-		ray_to_nearest = vector_sub(nearest, player->map_posi);
-		ray_length = vector_len(ray_to_nearest);
-
-		overlap = player->unit_size - ray_length;
-		if (overlap > 0)
+		//bot right
+		if (map->map[(fixed_x + 1) + (fixed_y - 1) * map->width] == '1')
 		{
-			ray_to_nearest = vector_norm(ray_to_nearest, ray_length);
-			player->map_posi = vector_sub(player->map_posi, vector_multi(ray_to_nearest, overlap));
+			//printf("posi start (%.3f, %.3f)\n", player->map_posi.x, player->map_posi.y);
+			nearest.x = ft_fmax(player->map_posi.x, (float)(fixed_x + 1));
+			nearest.y = ft_fmin(player->map_posi.y, (float)(fixed_y));
+			ray_to_nearest = vector_sub(nearest, player->map_posi);
+			ray_length = vector_len(ray_to_nearest);
+
+			overlap = player->unit_size - ray_length;
+			if (overlap > 0)
+			{
+				ray_to_nearest = vector_norm(ray_to_nearest, ray_length);
+				player->map_posi = vector_sub(player->map_posi, vector_multi(ray_to_nearest, overlap));
+			}
 		}
-	}
+
+
+
 
 	//printf("player->map_posi (%.3f, %.3f)\n", player->map_posi.x, player->map_posi.y);
 
